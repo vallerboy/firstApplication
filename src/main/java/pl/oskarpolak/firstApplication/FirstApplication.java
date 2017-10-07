e3432776f88ff6d96b3bd5c41adf98995b78d72b
Oskar <oskarbank@gmail.com>
2017-10-07 12:02:43 +0200

    asd

--- src/main/java/pl/oskarpolak/firstApplication/FirstApplication.java
+++ src/main/java/pl/oskarpolak/firstApplication/FirstApplication.java
@@ -2,11 +2,24 @@ package pl.oskarpolak.firstApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
+@Configuration
 public class FirstApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
+	}
+
+	@Bean(name = "messageSource")
+	public MessageSource messageSource(){
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		source.setDefaultEncoding("UTF-8");
+		return source;
 	}
 }
