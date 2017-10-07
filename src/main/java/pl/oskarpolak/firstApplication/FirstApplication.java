c34a62baa14bd111e486b63058e8dc265c09886a
Oskar <oskarbank@gmail.com>
2017-10-07 12:25:51 +0200

    asd

--- src/main/java/pl/oskarpolak/firstApplication/FirstApplication.java
+++ src/main/java/pl/oskarpolak/firstApplication/FirstApplication.java
@@ -6,20 +6,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@Configuration
-public class FirstApplication {
+public class FirstApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
 	}
 
-	@Bean(name = "messageSource")
-	public MessageSource messageSource(){
-		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-		source.setBasename("messages");
-		source.setDefaultEncoding("UTF-8");
-		return source;
-	}
 }
